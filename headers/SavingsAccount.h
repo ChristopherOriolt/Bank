68f4d87ece7fb3e613f27e4090263887c0a0c4e6
Christopher Oriolt <coriolt21@my.whitworth.edu>
2019-02-21 11:59:39 -0800

    added comment in savings account

---
+++ headers/SavingsAccount.h
@@ -0,0 +1,31 @@
+/////////////////////////////////////
+// Data Structures Project 1
+// Author: Christopher Oriolt
+// 2/20/2019
+/////////////////////////////////
+
+//wil this commit?
+#include "Account.h"
+
+#ifndef SavingsAccount_H_
+#define SavingsAccount_H_
+class SavingsAccount : public Account {
+public:
+    SavingsAccount(Customer c, double b) : Account(c, b) {
+        
+    }
+
+    void deposit(double amount) {
+        Balance = Balance + amount;
+    }
+
+    void withdrawal(double amount) {
+        Balance = Balance - amount;
+    }
+
+    void addInterest() {
+        
+    }
+};
+
+#endif
