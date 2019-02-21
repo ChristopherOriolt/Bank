67e50a6c69ed2042023bd2d18d372a97710e906f
Christopher Oriolt <coriolt21@my.whitworth.edu>
2019-02-20 21:06:48 -0800

    edited Account class

--- Bank/Bank/Account.cpp
+++ Bank/Bank/Account.cpp
@@ -1,3 +1,9 @@
+/////////////////////////////////////
+// Data Structures Project 1
+// Author: Christopher Oriolt
+// 2/20/2019
+/////////////////////////////////
+
 #include "Account.h"
 
 double Account::getBalance()
