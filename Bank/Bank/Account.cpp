763a11c5f7b13bcf087731929231dafa7196f938
Christopher Oriolt <coriolt21@my.whitworth.edu>
2019-02-20 20:40:11 -0800

    Created definitions for Account methods in Account.cpp

---
+++ Bank/Bank/Account.cpp
@@ -0,0 +1,24 @@
+#include "Account.h"
+
+double Account::getBalance()
+{
+	return 0.0;
+}
+
+string Account::getCustomer()
+{
+	return string();
+}
+
+string Account::toString()
+{
+	return string();
+}
+
+void Account::setCustomer(customer)
+{
+}
+
+void Account::SetBalance(double)
+{
+}
